a5e3d0bdb4e7d73fb8a544e48a190eb892d65ea1
MAD <mad@localhost.localdomain>
2017-11-23 13:47:07 -0500

    Add installation scripts

---
+++ docker-install.sh
@@ -0,0 +1,7 @@
+# exec with superuser permissions
+yum install -y yum-utils device-mapper-persistent-data lvm2 git
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce
+
+systemctl start docker
+systemctl enable docker
