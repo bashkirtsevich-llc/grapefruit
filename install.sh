801f3dc5b2d9dc751b7aa13129ca28527e62a0ea
MAD <mad@localhost.localdomain>
2017-11-25 14:00:57 -0500

    Update installation script

--- install.sh
+++ install.sh
@@ -1,18 +1,17 @@
-docker network create gf-net
-docker run -d -v $(pwd)/grapefruit-db:/data/db --restart unless-stopped --network gf-net --name mongodb mongo
+REPO_PATH=$(pwd)
 
+cd $(mktemp -d)
 git clone https://github.com/bashkirtsevich/grapefruit-web.git
-git clone https://github.com/bashkirtsevich/grapefruit-crawler.git
-
 cd grapefruit-web/
 docker build -t grapefruit-web .
 
-cd ..
+cd $(mktemp -d)
+git clone https://github.com/bashkirtsevich/grapefruit-crawler.git
 cd grapefruit-crawler/
 docker build -t grapefruit-crawler .
 
+docker network create gf-net
+docker run -d --restart unless-stopped --network gf-net -v $REPO_PATH/grapefruit-db:/data/db --name mongodb mongo
 docker run -d --restart unless-stopped --network gf-net --name grapefruit-web grapefruit-web
-docker run -d -p 6881:6881 --restart unless-stopped --network gf-net --name grapefruit-crawler grapefruit-crawler
-
-cd ..
-docker run -d -p 80:80 -v $(pwd)/nginx:/etc/nginx/ -v $(pwd)/nginx-logs:/var/log/nginx --network gf-net --name nginx nginx
+docker run -d --restart unless-stopped --network gf-net -p 6881:6881 --name grapefruit-crawler grapefruit-crawler
+docker run -d --restart unless-stopped --network gf-net -p 80:80 -v $REPO_PATH/nginx:/etc/nginx/ -v $REPO_PATH/nginx-logs:/var/log/nginx --name nginx nginx
