fc1a4a4a2778f6a0328837c211aeabba33d29ee6
MAD <mad@localhost.localdomain>
2017-11-25 13:07:31 -0500

    Remove "sudo" (user must be in "docker" group)

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
-sudo docker network create gf-net
-sudo docker run -d -v $(pwd)/grapefruit-db:/data/db --restart unless-stopped --network gf-net --name mongodb mongo
+docker network create gf-net
+docker run -d -v $(pwd)/grapefruit-db:/data/db --restart unless-stopped --network gf-net --name mongodb mongo
 
 git clone https://github.com/bashkirtsevich/grapefruit-web.git
 git clone https://github.com/bashkirtsevich/grapefruit-crawler.git
 
 cd grapefruit-web/
-sudo docker build -t grapefruit-web .
+docker build -t grapefruit-web .
 
 cd ..
 cd grapefruit-crawler/
-sudo docker build -t grapefruit-crawler .
+docker build -t grapefruit-crawler .
 
-sudo docker run -d --restart unless-stopped --network gf-net --name grapefruit-web grapefruit-web
-sudo docker run -d -p 6881:6881 --restart unless-stopped --network gf-net --name grapefruit-crawler grapefruit-crawler
+docker run -d --restart unless-stopped --network gf-net --name grapefruit-web grapefruit-web
+docker run -d -p 6881:6881 --restart unless-stopped --network gf-net --name grapefruit-crawler grapefruit-crawler
 
 cd ..
-sudo docker run -d -p 80:80 -v $(pwd)/nginx:/etc/nginx/ -v $(pwd)/nginx-logs:/var/log/nginx --network gf-net --name nginx nginx
+docker run -d -p 80:80 -v $(pwd)/nginx:/etc/nginx/ -v $(pwd)/nginx-logs:/var/log/nginx --network gf-net --name nginx nginx
