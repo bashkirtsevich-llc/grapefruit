ab82f9978162e1045b798c3e3def842b1836cfbe
MAD <mad@localhost.localdomain>
2017-11-25 13:05:04 -0500

    Adding current user to docker group

--- docker-install.sh
+++ docker-install.sh
@@ -1,7 +1,9 @@
-# exec with superuser permissions
+# exec with sudo
 yum install -y yum-utils device-mapper-persistent-data lvm2 git
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install docker-ce
 
 systemctl start docker
 systemctl enable docker
+
+usermod -aG docker $USER
