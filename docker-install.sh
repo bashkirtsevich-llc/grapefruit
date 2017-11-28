2912a11a9f02128c5204a9120c97fc7512a8e127
MAD <mad@localhost.localdomain>
2017-11-28 13:18:26 -0500

    Remove sudo

--- docker-install.sh
+++ docker-install.sh
@@ -1,5 +1,5 @@
 # exec with sudo
-yum install -y yum-utils device-mapper-persistent-data lvm2 git
+yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install docker-ce
 
--- dump.sh
+++ dump.sh
@@ -1,1 +1,1 @@
-sudo docker run --rm --name mongo-dump -v $(pwd)/dumps:/dumps --network gf-net --entrypoint mongodump mongo --host mongodb --db grapefruit --out /dumps
+docker run --rm --name mongo-dump -v $(pwd)/dumps:/dumps --network gf-net --entrypoint mongodump mongo --host mongodb --db grapefruit --out /dumps
