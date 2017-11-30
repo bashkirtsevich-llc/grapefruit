06212f1b4227983ebbba74823cce6761a3865bbd
MAD <mad@localhost.localdomain>
2017-11-30 13:02:41 -0500

    Remove user group updating

--- docker-install.sh
+++ docker-install.sh
@@ -1,9 +1,7 @@
 # exec with sudo
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce
+yum install -y docker-ce
 
 systemctl start docker
 systemctl enable docker
-
-usermod -aG docker $USER
