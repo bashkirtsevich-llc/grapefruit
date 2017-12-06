19364bd090b7eda6250a96399b13e39d83a15771
MAD <mad@localhost.localdomain>
2017-12-06 09:34:20 -0500

    Replace dockerfiles into this repository instead modules Committer: MAD <mad@localhost.localdomain>
    On branch master
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    	new file:   grapefruit-crawler/Dockerfile
    	new file:   grapefruit-web/Dockerfile
    	modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,14 @@
 REPO_PATH=$(pwd)
 
-cd $(mktemp -d)
-git clone https://github.com/bashkirtsevich/grapefruit-web.git
-cd grapefruit-web/
+cd $REPO_PATH/grapefruit-web/
 docker build -t grapefruit-web .
 
-cd $(mktemp -d)
-git clone https://github.com/bashkirtsevich/grapefruit-crawler.git
-cd grapefruit-crawler/
+cd $REPO_PATH/grapefruit-crawler/
 docker build -t grapefruit-crawler .
 
 docker network create gf-net
+
 docker run -d --restart unless-stopped --network gf-net -v $REPO_PATH/grapefruit-db:/data/db --name mongodb mongo
+docker run -d --restart unless-stopped --network gf-net -p 80:80 -v $REPO_PATH/nginx:/etc/nginx/ -v $REPO_PATH/nginx-logs:/var/log/nginx --name nginx nginx
 docker run -d --restart unless-stopped --network gf-net --name grapefruit-web grapefruit-web
 docker run -d --restart unless-stopped --network gf-net -p 6881:6881 --name grapefruit-crawler grapefruit-crawler
-docker run -d --restart unless-stopped --network gf-net -p 80:80 -v $REPO_PATH/nginx:/etc/nginx/ -v $REPO_PATH/nginx-logs:/var/log/nginx --name nginx nginx
