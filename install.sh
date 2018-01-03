650b1daf9c2a323e1c4dad030abc3a97dc633b86
D.A.Bashkirtsev <bashkirtsevich@users.noreply.github.com>
2018-01-03 23:34:45 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,4 +11,4 @@ docker network create gf-net
 docker run -d --restart unless-stopped --network gf-net -v $REPO_PATH/grapefruit-db:/data/db --name mongodb mongo
 docker run -d --restart unless-stopped --network gf-net -p 80:80 -v $REPO_PATH/nginx:/etc/nginx/ -v $REPO_PATH/nginx-logs:/var/log/nginx --name nginx nginx
 docker run -d --restart unless-stopped --network gf-net --name grapefruit-web grapefruit-web
-docker run -d --restart unless-stopped --network gf-net -p 6881:6881 --name grapefruit-crawler grapefruit-crawler
+docker run -d --restart unless-stopped --network gf-net -p 6881:6881/udp --name grapefruit-crawler grapefruit-crawler
