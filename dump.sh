b3eecef9da14cd85542e37074012e81f18dafe95
MAD <mad@localhost.localdomain>
2018-01-12 11:14:39 -0500

    Delete unused files/directories

--- dump.sh
+++
@@ -1,1 +0,0 @@
-docker run --rm --name mongo-dump -v $(pwd)/dumps:/dumps --network gf-net --entrypoint mongodump mongo --host mongodb --db grapefruit --out /dumps
--- mongo.sh
+++
@@ -1,1 +0,0 @@
-docker run -it --rm --name mongo --network gf-net --entrypoint mongo mongo --host mongodb
